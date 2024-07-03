9fd879d837ccb4047dca3412e2812c10e37fb74a
Lovely Tautuaa <lovelytau650@gmail.com>
2024-07-02 23:46:22 -0700

    Got main HTML elems by their ID

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,13 @@
 // Store key in variable
 
 const apiKey = "fa02be04f10180fa619c5d316e0baf31"
+
+//Get main HTML elements
+
+const userSearch = document.getElementById("user-search");
+
+const searchBtn = document.getElementById("search-btn");
+
+const cityList = document.getElementById("city-list");
+
+ const cityName = document.getElementById("city-name");
